714c333fee400d9f9f5d12068e4cf42a5f7ed5bb
Noel Ugwoke <35940208+leonnuxy@users.noreply.github.com>
2025-04-28 14:35:33 -0600

    Update test_search_jobs_live.py

--- tests/test_search_jobs_live.py
+++ tests/test_search_jobs_live.py
@@ -3,7 +3,7 @@ from lib import api_calls
 
 class TestSearchJobsLive(unittest.TestCase):
     def test_search_jobs_live(self):
-        api_key = "AIzaSyBvQONt17DUXCAogCsjjlXlRJKr2FASf1Y"
+        api_key = "YOUR_API_KEY"
         cse_id = "a078e663aec764a4b"
         query = "Python developer Calgary"
         results = api_calls.search_jobs(query, api_key, cse_id)
